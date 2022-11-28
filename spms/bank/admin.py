78448f3dc889762de371dd709cac6722262bdb72
Saiyara Raquib Oishy <83184006+OishyRaquib@users.noreply.github.com>
2022-11-28 22:06:16 +0600

    No operational error

--- spms/bank/admin.py
+++ spms/bank/admin.py
@@ -5,3 +5,7 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Question)
+admin.site.register(Department)
+admin.site.register(Program)
+admin.site.register(User)
+admin.site.register(Student)
--- spms/bank/models.py
+++ spms/bank/models.py
@@ -93,3 +93,39 @@ class Question(models.Model):
 #     Statement=models.CharField(max_length=200)
 #     SecID=models.ForeignKey(Section)
 
+#models
+
+class Department(models.Model):
+    deptID=models.CharField(max_length=15)
+    deptName=models.CharField(max_length=50)
+
+
+class Program(models.Model):
+    programID=models.CharField(max_length=5)
+    programName=models.CharField(max_length=50,null=False)
+    department=models.ForeignKey(Department,on_delete=models.CASCADE)
+
+
+class User(models.Model):
+    # TYPES=(('Faculty','Faculty'),
+    # ('Student','Student')    
+    # )
+    userID=models.CharField(max_length=10,unique=True)
+    name=models.CharField(max_length=100)
+    dateofbirth=models.DateField()
+    email=models.CharField(max_length=50,blank=True)
+    phone=models.IntegerField(blank=True)
+    address=models.CharField(max_length=100,blank=True)
+
+    def __str__(self):
+        return self.name
+    # usertype=models.Model(choices=TYPES)
+    # def Meta(self):
+    #     return self.userID
+
+class Student(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    SUserID=models.CharField(max_length=7)
+    enroll_year=models.IntegerField()
+    programID=models.ForeignKey(Program,on_delete=models.CASCADE)
+    deptID=models.ForeignKey(Department,on_delete=models.CASCADE)
